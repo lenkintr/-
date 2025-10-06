6f49b4656256413761a3cff24ec82a2e147f5d6a
lenkintr <61546924+lenkintr@users.noreply.github.com>
2025-10-06 09:02:50 +0500

    Create project.cs

---
+++ project.cs
@@ -0,0 +1,13 @@
+csharp
+// Инициализация среды
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+// Настройка вывода
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("🚀 Рабочая тетрадь по C# готова к использованию!");
